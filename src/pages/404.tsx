60ba7f1663a179def0f4bea00dcf8d25d18abb91
Nelson Chinedu <egwuchinedu69@gmail.com>
2020-07-07 16:22:36 +0100

    add 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,17 @@
+import React, { FunctionComponent } from 'react';
+import Link from 'next/link';
+
+const PageNotFound: FunctionComponent<{}> = () => {
+  return (
+    <div className="mt-20 text-center">
+      <h2>Page Not Found</h2>
+      <Link href="/">
+        <a className="text-blue-400 underline hover:no-underline">
+          Return to homepage
+        </a>
+      </Link>
+    </div>
+  )
+};
+
+export default PageNotFound;
