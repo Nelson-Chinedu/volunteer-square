00df08639a9e0705f3f13dbbd2ba17b2be749469
Nelson Chinedu <egwuchinedu69@gmail.com>
2020-07-07 16:08:23 +0100

    add form input validation

--- src/components/AppLayout/Profile/ProfileForm.tsx
+++ src/components/AppLayout/Profile/ProfileForm.tsx
@@ -1,85 +1,124 @@
-import React, { FunctionComponent, useState } from 'react';
-import { Form } from 'antd';
+import React, { FunctionComponent } from 'react';
+import { Spin } from 'antd';
+import { LoadingOutlined } from '@ant-design/icons';
+import {useFormik} from 'formik';
+import * as yup from 'yup';
 
 import InputForm from 'src/components/SharedLayout/Shared/Input';
 import Button from 'src/components/SharedLayout/Shared/Button';
 
-const ProfileForm: FunctionComponent<{}> = () => {
-  const [ form ] = Form.useForm();
-  const [ formLayout, setFormLayout ] = useState('vertical');
+const validationSchema = yup.object().shape({
+  firstname: yup.string()
+    .required('Required'),
+  lastname: yup.string()
+    .required('Required'),
+  email: yup.string()
+    .required('Required')
+    .email('Enter a valid E-mail Address'),
+  phone: yup.string()
+    .required('Required'),
+  password: yup.string()
+    .required('Required')
+    .min(8, 'Password must be 8 characters long'),
+});
 
-  const _onFormLayoutChange = ({ layout}: any) => {
-    setFormLayout(layout);
+const ProfileForm: FunctionComponent<{}> = () => {
+
+  const _handleProfile = () => {
+    console.log('clicked');
   };
 
-  const formItemLayout = formLayout === 'vertical'
-    ?
-      {
-        labelCol: {
-          span: 24
-        },
-        wrapperCol: {
-          span: 48
-        },
-      } : null;
+  const formik = useFormik({
+    initialValues: {
+      firstname: '',
+      lastname: '',
+      email: '',
+      phone: '',
+      password: '',
+    },
+    onSubmit: _handleProfile,
+    validationSchema
+  });
 
-  const buttonItemLayout = formLayout === 'vertical' ?
-    {
-      wrapperCol: {
-        span: 48,
-      },
-    } : null;
+  const {
+    values,
+    handleBlur,
+    handleChange,
+    handleSubmit,
+    isSubmitting,
+    errors
+  } = formik;
 
   return (
     <div className="my-5 w-3/5 m-auto c-profileForm">
-      <Form
-        { ...formItemLayout }
-        layout = "vertical"
-        form = { form }
-        initialValues = {{ layout: formLayout }}
-        onValuesChange = { _onFormLayoutChange }
+      <form
+        onSubmit={handleSubmit}
         className = " c-profileForm-container"
       >
         <div className="flex justify-between w-full c-profileForm-fullname">
           <InputForm
             label = "Firstname"
             placeholder = "Enter Firstname"
+            name="firstname"
+            value={values.firstname}
+            onChange={handleChange}
+            onBlur={handleBlur}
             type="text"
             size="large"
+            error={errors.firstname}
           />
           <InputForm
             label = "Lastname"
             placeholder = "Enter Lastname"
+            name="lastname"
+            value={values.lastname}
+            onChange={handleChange}
+            onBlur={handleBlur}
             type="text"
             size="large"
+            error={errors.lastname}
           />
         </div>
         <InputForm
           label = "Email Address"
           placeholder = "Enter Email Address"
+          name="email"
+          value={values.email}
+          onChange={handleChange}
+          onBlur={handleBlur}
           type="text"
           size="large"
+          error={errors.email}
         />
         <InputForm
           label = "Phone Number"
           placeholder = "Enter Phone Number"
+          name="phone"
+          value={values.phone}
+          onChange={handleChange}
+          onBlur={handleBlur}
           type = "tel"
           size = "large"
+          error={errors.phone}
         />
         <InputForm
           label = "Password (Leave blank, if using current password)"
           placeholder = "Enter Password"
+          name="password"
+          value={values.password}
+          onChange={handleChange}
+          onBlur={handleBlur}
           type = "password"
           size = "large"
+          error={errors.password}
         />
         <Button
-          item = {buttonItemLayout}
           type = "submit"
           className = "w-full bg-blue-700 rounded text-white p-3 mt-6 mb-2"
         >
-          Save
+          {isSubmitting ? <Spin indicator={<LoadingOutlined className="text-white" />} /> : 'Save'}
         </Button>
-      </Form>
+      </form>
     </div>
   )
 };
